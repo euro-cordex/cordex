499db21c05d538422da7e3b1ff8f4d46650df261
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2020-02-23 16:20:30 +0100

    Update domain.py

--- examples/domain.py
+++ examples/domain.py
@@ -1,10 +1,17 @@
-
 from cordex import domain as dm
 
-domains = dm.domains()
-print('available domains: {}'.format(domains))
+# available tables
+print(dm.tables())
+# print cordex core table
+print(dm.table('cordex-core'))
+# available domains
+print(dm.domains())
+# available domains in cordex-core
+print(dm.domains('cordex-core'))
 
-for short_name in domains:
+# create domains with some dummy data (uses cdo topo)
+for short_name in dm.domains():
     print('creating domain: {}'.format(short_name))
     domain = dm.domain(short_name)
+    print(domain)
     domain.to_netcdf(short_name+'.nc', dummy='topo')
